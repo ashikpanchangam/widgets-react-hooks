963615e455e09c292821eeb2e934e6020b1dbd14
Ashik Panchangam Jaikishan <ashikpanchangam@gmail.com>
2020-08-12 20:31:08 -0700

    expanding the accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,27 @@
 import React,  {useState} from 'react'
 
 const Accordion = ({items}) => {
+    // Array destructuring
     const [activeIndex, setActiveIndex] = useState(null);
 
     const onTitleClick = (index) => {
         setActiveIndex(index);
-    } 
+    }
 
     const renderedItems = items.map((item, index) => {
+
+        // Add the class active if the index clicked is activeIndex
+        const active = index === activeIndex ? 'active' : ''; 
+
         return <React.Fragment key={item.title}>
             <div 
-                className="title active"
+                className={`title ${active}`}
                 onClick = {() => onTitleClick(index)}
                 >
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
         </React.Fragment>
@@ -24,7 +29,6 @@ const Accordion = ({items}) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
-            <h1>{activeIndex}</h1>
         </div>
     );
 }
